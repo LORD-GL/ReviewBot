224fa781a2bbc62e1e793dcc4beef467e413bc02
LORD-GL <boikogv06@gmail.com>
2022-02-10 13:19:00 +0200

    add "What can" | date in data.txt

--- main.py
+++ main.py
@@ -6,11 +6,15 @@
     ver. 12.04.2021 (MM.DD.YYYY)
     https://t.me/LORD_GL 
 """
-import telebot
+import telebot, datetime
 
 HTTP_API = "5056002443:AAHDdOkl2s0xPSrAKL0Hj85BkbMsdpNd1yQ"
 
 bot = telebot.TeleBot(HTTP_API)
+
+REV_BUTTON_TEXT = "New Review"
+rev_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+rev_keyboard.row(REV_BUTTON_TEXT)
 
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -20,12 +24,22 @@ Leave your review:
     """
     bot.send_message(message.chat.id, IntroText)
 
+@bot.message_handler(func = lambda message: message.text == REV_BUTTON_TEXT)
+def new_rev(message):
+    bot.send_message(message.chat.id, "Send new review:")
+
 @bot.message_handler(content_types=["text"])
 def writeRev(message):
-    file = open('data.txt', 'a')
-    file.write(f"Name: {message.chat.username}\n")
-    file.write(f"id: {message.chat.id}\n")
-    file.write(f"date: {message.text}\n----\n")
-    file.close()
+    try:
+        file = open('data.txt', 'a')
+        file.write(f"Name: {message.chat.username}\n")
+        file.write(f"id: {message.chat.id}\n")
+        file.write(f"Text: {message.text}\n")
+        file.write(f"Date: {datetime.datetime.now()}\n----\n")
+        file.close()
+        bot.send_message(message.chat.id, "Thank for your review!\n", reply_markup = rev_keyboard)
+    except:
+        bot.send_message(message.chat.id, "Something went wrong!", reply_markup = rev_keyboard)
+
 
 bot.polling(none_stop=True, interval=0)
